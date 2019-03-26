74e908a13a4df8cda8cc874535afae848031ed07
kruemmelkhanh <khanh.truong@hotmail.de>
2019-03-26 17:16:54 +0100

    mine

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 class Calc {
   constructor(num) {
-    this.num = num
-  }
-  add(numToAdd) {
-    this.throwOnBadDatatype(numToAdd)
-    this.num = this.num + numToAdd
-    return this
+    this.num = num;
   }
 
-  throwOnBadDatatype(numToAdd) {
-    if (typeof numToAdd === "string") {
-      throw new Error("No strings allowed")
+  add(numToAdd) {
+    this.throwOnBadDatatype(numToAdd);
+    this.num += numToAdd;
+    return this;
+  }
+
+  sub(numToSub) {
+    this.throwOnBadDatatype(numToSub);
+    this.num -= numToSub;
+    return this;
+  }
+
+  mult(numToMult) {
+    this.throwOnBadDatatype(numToMult);
+    this.num *= numToMult;
+    return this;
+  }
+
+  res() {
+    return this.num;
+  }
+
+  div(numToDiv) {
+    this.throwOnBadDatatype(numToDiv);
+    this.num /= numToDiv;
+    return this;
+  }
+
+  throwOnBadDatatype(num) {
+    if (typeof num === "string") {
+      throw new Error("No strings allowed");
     }
 
-    if (typeof numToAdd === "object") {
-      throw new Error("No object allowed")
+    if (typeof num === "object") {
+      throw new Error("No object allowed");
+    }
+
+    if (typeof num === "boolean") {
+      throw new Error("No boolean allowed");
     }
   }
 }
 
-module.exports = Calc
+module.exports = Calc;
--- index.test.js
+++ index.test.js
@@ -1,21 +1,53 @@
-const Calc = require("./index")
+const Calc = require("./index");
 
 describe("Calculator tests", () => {
   test("should be able to add 1 + 1", () => {
-    expect(new Calc(1).add(1)).toEqual({ num: 2 })
-  })
+    expect(new Calc(1).add(1).res()).toEqual(2);
+  });
+
+  test("should be able to substract 5 - 6", () => {
+    expect(new Calc(5).sub(6).res()).toEqual(-1);
+  });
+
+  test("should be able to multiply 2 * 5", () => {
+    expect(new Calc(2).mult(5).res()).toEqual(10);
+  });
+
+  test("should be able to divide 15 * 4", () => {
+    expect(new Calc(15).div(4).res()).toEqual(3.75);
+  });
 
   test("should be able to add multiple numbers", () => {
-    expect(new Calc(1).add(1).add(1)).toEqual({ num: 3 })
-  })
+    expect(
+      new Calc(1)
+        .add(1)
+        .add(5)
+        .res()
+    ).toEqual(7);
+  });
+
+  test("should be able to do multiple thinks", () => {
+    expect(
+      new Calc(5)
+        .add(6)
+        .sub(1)
+        .mult(10)
+        .div(2)
+        .res()
+    ).toEqual(50);
+  });
 
   test("Dont accept strings", () => {
-    expect(() => new Calc(100).add("onehundret")).toThrow("No strings allowed")
-  })
+    expect(() => new Calc(100).add("onehundret")).toThrow("No strings allowed");
+  });
 
   test("Dont accept object", () => {
     expect(() => new Calc(100).add({ num: "onehundret" })).toThrow(
       "No object allowed"
-    )
-  })
-})
+    );
+  });
+
+  test("Dont accept boolean", () => {
+    expect(() => new Calc(100).add(true)).toThrow("No boolean allowed");
+  });
+});
